19f072c983a5454456ae614f9d534577d9a3825d
Rotakon <rotakon-3@yandex.ru>
2020-03-17 08:32:28 +0500

    ghsrtjugk

---
+++ шизофрения/game/js.js
@@ -0,0 +1,41 @@
+const CANVAS_BORDER_COLOUR = 'black';
+const CANVAS_BACKGROUND_COLOUR = "white";
+const SNAKE_COLOUR = 'lightgreen';
+const SNAKE_BORDER_COLOUR = 'darkgreen';
+const FOOD_COLOUR = 'red';
+const FOOD_BORDER_COLOUR = 'darkred';
+
+
+
+
+snake = [
+      {x: 150, y: 150},
+      {x: 140, y: 150},
+      {x: 130, y: 150},
+      {x: 120, y: 150},
+      {x: 110, y: 150}
+    ];
+	
+	
+
+
+const game = document.getElementById('game');
+
+document.addEventListener('keydown', changeDirection);
+
+function changeDirection(event) {
+	if (check){
+		clearInterval(timer);
+	}
+	timer = setInterval(loop, 100, event);
+}
+
+
+function randomTen(min, max) {
+      return Math.round((Math.random() * (max-min) + min) / 10) * 10;	  
+    }
+
+
+
+
+
